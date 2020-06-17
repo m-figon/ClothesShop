fc0d2ed579ba09d8101041d1ba1b804a6bede4e8
mateusz <dalvan234@gmail.com>
2020-06-17 19:58:56 +0200

    displaying products in details component for now only for man

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  type;
+  man;
+  content;
   ngOnInit(): void {
+    this.http.get<any>('https://rocky-citadel-32862.herokuapp.com/ClothesShop/man').subscribe(data => {
+      //console.log(data);
+      this.man = data;
+      console.log(this.man);
+      this.type = (this.route.snapshot.paramMap.get('details'));
+      console.log(this.type);
+      for(let item of this.man){
+        if(item.type===this.type){
+          this.content=item.content.slice();
+          console.log(this.content);
+        }
+      }
+    })
+      
+  
   }
 
 }
