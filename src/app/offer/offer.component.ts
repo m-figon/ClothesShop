bbca5bf08dd53d617082c1f031e0a1cdb38460db
mateusz <dalvan234@gmail.com>
2020-06-18 23:07:27 +0200

    items with certain condition displayed in offer component, link to details works from choice and offer components now, little appearance changes in offer and choice components

--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-offer',
@@ -8,20 +9,36 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class OfferComponent implements OnInit {
   type;
+  lastType;
   ad;
-  constructor(private route: ActivatedRoute) { }
+  content;
+  change;
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
-    setInterval(()=>{
+    setInterval(() => {
+      this.lastType=this.type;
       this.type = (this.route.snapshot.paramMap.get('type'));
-    if(this.type==="man"){
-      this.ad="assets/img/man.jpg"
-    }
-    if(this.type==="woman"){
-      this.ad="assets/img/woman.jpg"
-    }
-    },500)
-    
+      if (this.type === "man" && (this.lastType==="woman" || !this.lastType)) {
+        this.ad = "assets/img/man.jpg"
+        this.http.get<any>('https://rocky-citadel-32862.herokuapp.com/ClothesShop/' + this.type).subscribe(data => {
+          //console.log(data);
+          this.content = data;
+          this.change = true;
+          console.log(this.content);
+        })
+      }
+      if (this.type === "woman"  && (this.lastType==="man" || !this.lastType)) {
+        this.ad = "assets/img/woman.jpg"
+        this.http.get<any>('https://rocky-citadel-32862.herokuapp.com/ClothesShop/' + this.type).subscribe(data => {
+          //console.log(data);
+          this.content = data;
+          this.change = true;
+          console.log(this.content);
+        })
+      }
+    }, 500)
+
   }
 
 }
