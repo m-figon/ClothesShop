c1f4cd896a13355f2950868770d2f3311de365d5
mateusz <dalvan234@gmail.com>
2020-06-24 10:41:13 +0200

    type of clothes and order-setting bars select effect change, settings component appearance changed, settings component validation added and additional button, orders appearance changed, height of settings background adjusting to screen size

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -9,22 +9,6 @@ export class AppService {
   constructor() { }
   setAccount(value){
     this.account=value;
-    console.log(value);
-  }
-  sendToCart(value){
-    /*
-    for(let item of this.cart){
-      if(item.name===value.name && item.size===value.size){
-        value.quantity++;
-        console.log(this.cart);
-        return;
-      }
-    }
-    */
-   console.log(value);
-    this.cart.push(value);
-    console.log(this.cart);
-    return;
   }
   getCart(){
     return this.cart;
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -8,7 +8,7 @@ import { IfStmt } from '@angular/compiler';
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit,AfterViewInit {
+export class SettingsComponent implements OnInit, AfterViewInit {
 
   constructor(private http: HttpClient, private appService: AppService) { }
   users;
@@ -17,11 +17,9 @@ export class SettingsComponent implements OnInit,AfterViewInit {
   email;
   name;
   surname;
-  currentPassword="";
-  newPassword="";
-  confirmPassword="";
-  changingData=true;
-  changingPassword=false;
+  currentPassword = "";
+  newPassword = "";
+  confirmPassword = "";
   loadingFinished;
   ngOnInit(): void {
     this.http.get<any>('https://rocky-citadel-32862.herokuapp.com/ClothesShop/users').subscribe(data => {
@@ -33,18 +31,34 @@ export class SettingsComponent implements OnInit,AfterViewInit {
         if (item.email === this.appService.getAccount()) {
           this.id = item.id;
           console.log(this.users[this.id]);
-          this.email=this.users[this.id].email;
-          this.name=this.users[this.id].name;
-          this.surname=this.users[this.id].surname;
+          this.email = this.users[this.id].email;
+          this.name = this.users[this.id].name;
+          this.surname = this.users[this.id].surname;
         }
       }
     })
   }
   ngAfterViewInit() {
-    this.loadingFinished=true;
+    this.loadingFinished = true;
   }
-  changeData(){
-    if(!this.changingPassword && this.changeData){
+  changeData() {
+    let correctFlag = true;
+    if (!(this.email.match(/^[a-z0-9\._\-]+@[a-z0-9\.\-]+\.[a-z]{2,4}$/) === null)) {
+      console.log('email correct');
+    } else {
+      correctFlag = false;
+    }
+    if (!(this.name.match(/^[a-zA-Z0-9\.\-_]{4,10}$/) === null)) {
+      console.log('name correct');
+    } else {
+      correctFlag = false;
+    }
+    if (!(this.surname.match(/^[a-zA-Z0-9\.\-_]{4,10}$/) === null)) {
+      console.log('surname correct');
+    } else {
+      correctFlag = false;
+    }
+    if (correctFlag) {
       this.http.put<any>("https://rocky-citadel-32862.herokuapp.com/ClothesShop/users/" + this.id, {
         email: this.email,
         name: this.name,
@@ -57,31 +71,17 @@ export class SettingsComponent implements OnInit,AfterViewInit {
         console.log(data);
         alert('user data changed');
       })
+    }else{
+      alert('wrong data')
     }
-    if(this.changingPassword && this.changeData){
-      if(this.users[this.id].password===this.currentPassword && this.newPassword===this.confirmPassword){
-        this.http.put<any>("https://rocky-citadel-32862.herokuapp.com/ClothesShop/users/" + this.id, {
-        email: this.email,
-        name: this.name,
-        surname: this.surname,
-        password: this.newPassword,
-        cart: this.users[this.id].cart,
-        orders: this.users[this.id].orders,
-        id: this.users[this.id].id
-      }).toPromise().then(data => {
-        console.log(data);
-        alert('user password changed');
-      })
-      }else{
-        alert("You chose wrong password")
-      }
-    }
-    if(this.changingPassword && !this.changeData){
-      if(this.users[this.id].password===this.currentPassword && this.newPassword===this.confirmPassword){
-        this.http.put<any>("https://rocky-citadel-32862.herokuapp.com/ClothesShop/users/" + this.id, {
+
+  }
+  changePassword() {
+    if (this.users[this.id].password === this.currentPassword && this.newPassword === this.confirmPassword && !(this.newPassword.match(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\.\-_@$!%*#?&])[A-Za-z\d\.\-_@$!%*#?&]{8,13}$/) === null)) {
+      this.http.put<any>("https://rocky-citadel-32862.herokuapp.com/ClothesShop/users/" + this.id, {
         email: this.users[this.id].email,
         name: this.users[this.id].name,
-        surname:this.users[this.id].surname,
+        surname: this.users[this.id].surname,
         password: this.newPassword,
         cart: this.users[this.id].cart,
         orders: this.users[this.id].orders,
@@ -90,11 +90,8 @@ export class SettingsComponent implements OnInit,AfterViewInit {
         console.log(data);
         alert('user password changed');
       })
-      }else{
-        alert("You chose wrong password")
-      }
+    } else {
+      alert("You chose wrong password")
     }
-        
   }
-
 }
